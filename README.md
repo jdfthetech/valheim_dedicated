0d19baf70121f8d0794de6507ab381cd5a7165fe
jdfthetech <jdf@onlinemarketspecialist.com>
2021-02-28 09:55:45 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Valheim Dedicated Linux Files
+
+These are the files for the Valheim Dedicated server video.
+Simply clone these, make sure the valheimserver.sh file is executable and you are ready to go.
+
---
+++ valheim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+# Tip: Make a local copy of this script to avoid it being overwritten by steam.
+# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+# NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
+steamcmd +login anonymous +force_install_dir /home/username/Valheim +app_update 896660 +quit
+
+./valheim_server.x86_64 -name "<Server Name>" -port 2457 -world "Dedicated" -password "<The Password>" -public 1 > /dev/null &
+
+export LD_LIBRARY_PATH=$templdpath
+
+echo "Server started"
+echo ""
+#read -p "Press RETURN to stop server"
+#echo 1 > server_exit.drp
+
+#echo "Server exit signal set"
+#echo "You can now close this terminal"
+
+while :
+do
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+echo "valheim.service: timestamp ${TIMESTAMP}"
+sleep 60
+done
+
