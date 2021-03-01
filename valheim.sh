7ec8109311062c576135360b15a96a5443ce1e18
jdfthetech <jdf@onlinemarketspecialist.com>
2021-03-01 06:03:02 -0800

    updated username to <username> in valheim.sh

--- valheim.sh
+++ valheim.sh
@@ -7,7 +7,7 @@ export SteamAppId=892970
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-steamcmd +login anonymous +force_install_dir /home/username/Valheim +app_update 896660 +quit
+steamcmd +login anonymous +force_install_dir /home/<username>/Valheim +app_update 896660 +quit
 
 ./valheim_server.x86_64 -name "<Server Name>" -port 2457 -world "Dedicated" -password "<The Password>" -public 1 > /dev/null &
 
