beb4b58b1a33771740c307e13be9492a5d1b61c4
jdfthetech <jdf@onlinemarketspecialist.com>
2021-03-01 06:08:52 -0800

    added line for manual installs to valheim.sh

--- valheim.sh
+++ valheim.sh
@@ -9,6 +9,11 @@ export SteamAppId=892970
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
 steamcmd +login anonymous +force_install_dir /home/<username>/Valheim +app_update 896660 +quit
 
+#NOTE: if you manually installed steamcmd, comment out the above line and uncomment below.
+# change this to the location of your steamcmd.sh file if you did a manual install:
+
+#<DIRECTORY>/steamcmd.sh +login anonymous +force_install_dir /home/<username>/Valheim +app_update 896660 +quit
+
 ./valheim_server.x86_64 -name "<Server Name>" -port 2457 -world "Dedicated" -password "<The Password>" -public 1 > /dev/null &
 
 export LD_LIBRARY_PATH=$templdpath
